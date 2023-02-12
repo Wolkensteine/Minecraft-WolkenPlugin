b004171a25ca58355429f88cdb4262e9c5d72159
Wolkensteine <bokaj.bb22@gmail.com>
2023-02-12 17:18:56 +0100

    Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,17 @@
 package Main;
 
-public class Main extends JavaPlugins {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+
+	public void onEnable () {
+		System.out.println("Wolkenplugin> Enabeling ...");
+		System.out.println("Wolkenplugin> Enabled.");
+	}
 	
+	public void onDisable () {
+		System.out.println("Wolkenplugin> Disabeling ...");
+		System.out.println("Wolkenplugin> Disabled.");
+	}
 
 }
