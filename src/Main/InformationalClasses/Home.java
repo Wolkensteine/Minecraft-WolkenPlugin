5a5310766babc7dd862bfe23da508e5e3d07a365
Wolkensteine <bokaj.bb22@gmail.com>
2023-02-12 17:57:15 +0100

    More functions

---
+++ src/Main/InformationalClasses/Home.java
@@ -0,0 +1,25 @@
+package Main.InformationalClasses;
+
+public class Home {
+
+	public static String ownerN;
+	public static String oID;
+	public static int XLocationMidChunk;
+	public static int YLocationMidChunk;
+	
+	public Home(String ownerName, String ownerID, int midLocationX, int midLocationY) {
+		ownerN = ownerName;
+		oID = ownerID;
+		XLocationMidChunk = midLocationX;
+		YLocationMidChunk = midLocationY;
+	}
+	
+	public static void isPartOfHome(int X, int Y) {
+		
+	}
+	
+	public static void isAllowed(String playerID) {
+		
+	}
+	
+}
---
+++ src/Main/utils/Variables.java
@@ -0,0 +1,9 @@
+package Main.utils;
+
+import Main.InformationalClasses.Home;
+
+public class Variables {
+
+	public static Home[] Homes;		// Array of homes
+	
+}
